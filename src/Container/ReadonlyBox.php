0a46898837ada573bd263d0b65b583d1a6f4c01c
ascetik <ascetik@free.fr>
2023-12-05 20:35:08 +0100

    doc

--- src/Container/ReadonlyBox.php
+++ src/Container/ReadonlyBox.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * This is part of the Ascetik objectstorage package
+ *
+ * @package    ObjectStorage
+ * @category   Container
+ * @license    https://opensource.org/license/mit/  MIT License
+ * @copyright  Copyright (c) 2023, Vidda
+ * @author     Vidda <vidda@ascetik.fr>
+ */
+
+declare(strict_types=1);
+
 namespace Ascetik\ObjectStorage\Container;
 
 use Ascetik\ObjectStorage\Traits\ReadableContainer;
@@ -11,7 +23,5 @@ class ReadonlyBox
 
     public function __construct(private SplObjectStorage $container)
     {
-        
     }
-
 }
