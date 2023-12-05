53cad5b8efbf27d9190eb16131170745229c5a3e
ascetik <ascetik@free.fr>
2023-12-05 20:05:51 +0100

    finished implementation for Box
    and tests

--- src/Container/Box.php
+++ src/Container/Box.php
@@ -1,7 +1,20 @@
 <?php
 
+/**
+ * This is part of the Ascetik objectstorage package
+ *
+ * @package    ObjectStorage
+ * @category   Container
+ * @license    https://opensource.org/license/mit/  MIT License
+ * @copyright  Copyright (c) 2023, Vidda
+ * @author     Vidda <vidda@ascetik.fr>
+ */
+
+declare(strict_types=1);
+
 namespace Ascetik\ObjectStorage\Container;
 
+use Closure;
 use SplObjectStorage;
 
 class Box implements \Countable,  \IteratorAggregate
@@ -13,16 +26,15 @@ class Box implements \Countable,  \IteratorAggregate
         $this->container = new SplObjectStorage();
     }
 
-    public function isEmpty():bool
+    public function isEmpty(): bool
     {
         return $this->container->count() == 0;
     }
 
     public function contains(object $instance)
     {
-        foreach($this->container as $content)
-        {
-            if($instance === $content){
+        foreach ($this->container as $content) {
+            if ($instance === $content) {
                 return true;
             }
         }
@@ -31,25 +43,122 @@ class Box implements \Countable,  \IteratorAggregate
 
     public function hasAny(object $instance)
     {
-        foreach($this->container as $content)
-        {
-            if($instance == $content){
+        foreach ($this->container as $content) {
+            if ($instance == $content) {
                 return true;
             }
         }
         return false;
     }
 
-    public function set(object $instance, mixed $offset = null): void
+    public function push(object $instance, mixed $offset = null): void
     {
         $this->container->attach($instance, $offset);
+    }
+
+    public function first()
+    {
+        $this->container->rewind();
+        return $this->container->current();
+    }
+
+
+    public function last()
+    {
+        while ($this->container->valid()) {
+            $next = $this->container->current();
+            $this->container->next();
+        }
+        return $next;
+    }
+    public function unshift(object $instance, mixed $offset = null): void
+    {
+        $container = new SplObjectStorage();
+        $container->attach($instance, $offset);
+        $container->addAll($this->container);
+        $this->container = $container;
+    }
+
+    public function remove(Closure $callback)
+    {
+        $content = $this->find($callback);
+        if ($content) {
+            $this->container->detach($content);
+            return true;
+        }
+        return false;
+    }
+
+    public function shift(): object
+    {
+        $current = $this->first();
+        $this->container->detach($current);
+        return $current;
+    }
+
+    public function pop()
+    {
+        $next = $this->last();
+        $this->container->detach($next);
+        return $next;
+    }
+
+    public function find(Closure $closure): ?object
+    {
+        foreach ($this->container as $content) {
+            $result = call_user_func($closure, $content);
+            if ($result) {
+                return $content;
+            }
+        }
+        return null;
+    }
+
+    public function filter(Closure $callback): self
+    {
+        $output = new self();
+        foreach ($this->container as $content) {
+            $result = call_user_func($callback, $content);
+            if ($result) {
+                $offset = $this->container->offsetGet($content);
+                $output->push($content, $offset);
+            }
+        }
+        return $output;
+    }
+
+    public function each(Closure $closure)
+    {
+        foreach ($this->container as $content) {
+            call_user_func($closure, $content);
+        }
+    }
+
+    public function map(Closure $closure): self
+    {
+        $output = new self();
+        foreach ($this->container as $content) {
+            $result = call_user_func($closure, $content);
+            $output->push($result);
+        }
+
+        return $output;
+    }
+
+    public function toArray(): array
+    {
+        $output = [];
+        foreach ($this->container as $content) {
+            $output[] = $content;
+        }
+        return $output;
     }
 
     public function clear(): void
     {
         $this->container->removeAll($this->container);
     }
-    
+
     public function count(): int
     {
         return $this->container->count();
--- tests/BoxTest.php
+++ tests/BoxTest.php
@@ -22,72 +22,101 @@ class BoxTest extends TestCase
         $this->assertTrue($this->storage->isEmpty());
     }
 
-    public function testBoxContent()
+    public function testPushingContent()
     {
         $essai1 = new FakeInstance('one');
         $essai2 = new FakeInstance('two');
         $essai3 = clone $essai1;
-        $this->storage->set($essai1);
-        $this->storage->set($essai2);
+        $this->storage->push($essai1);
+        $this->storage->push($essai2);
         $this->assertCount(2, $this->storage);
         $this->assertTrue($this->storage->contains($essai1));
         $this->assertFalse($this->storage->contains(new stdClass()));
         $this->assertTrue($this->storage->hasAny($essai3));
     }
-/*
+
     public function testRemoveSomeContent()
     {
-        $this->storage->set(new FakeInstance('test'));
-        $this->storage->set(new FakeInstance('essai'));
-        $this->storage->set(new FakeInstance('another try'));
+        $this->storage->push(new FakeInstance('test'));
+        $this->storage->push(new FakeInstance('essai'));
+        $this->storage->push(new FakeInstance('another try'));
         $this->assertCount(3, $this->storage);
 
-        $this->assertTrue($this->storage->remove(fn(FakeInstance $instance) => $instance->name == 'essai'));
-        // $this->assertFalse($this->storage->remove(new Essai('portnaouak')));
+        $this->assertTrue($this->storage->remove(fn (FakeInstance $instance) => $instance->name == 'essai'));
+        $this->assertFalse($this->storage->remove(fn (FakeInstance $instance) => $instance->name == 'something else'));
         $this->assertCount(2, $this->storage);
     }
 
     public function testGetFirstElementFromStorage()
     {
-        $this->storage->attach(new Essai('test'));
-        $this->storage->attach(new Essai('essai'));
-        $shifted = $this->storage->shift();
+        $this->storage->push(new FakeInstance('test'));
+        $this->storage->push(new FakeInstance('essai'));
+        $this->assertCount(2, $this->storage);
+        $shifted = $this->storage->first();
 
-        $this->assertInstanceOf(Essai::class, $shifted);
+        // $this->assertInstanceOf(FakeInstance::class, $shifted);
         $this->assertSame('test', $shifted->name);
+        // $this->assertCount(1, $this->storage);
     }
 
     public function testGetLastElementFromStorage()
     {
-        $this->storage->attach(new Essai('test'));
-        $this->storage->attach(new Essai('another try'));
-        $this->storage->attach(new Essai('essai'));
+        $this->storage->push(new FakeInstance('test'));
+        $this->storage->push(new FakeInstance('essai'));
+        $shifted = $this->storage->last();
+        $this->assertSame('essai', $shifted->name);
+    }
+
+    public function testUnshiftingContent()
+    {
+        $this->storage->push(new FakeInstance('second'));
+        $this->storage->push(new FakeInstance('third'));
+        $this->storage->unshift(new FakeInstance('first'));
+        $this->assertSame('first', $this->storage->first()->name);
+        $this->assertSame('third', $this->storage->last()->name);
+    }
+
+    public function testShouldShiftFirstElementFromStorage()
+    {
+        $this->storage->push(new FakeInstance('test'));
+        $this->storage->push(new FakeInstance('essai'));
+        $this->assertCount(2, $this->storage);
+        $shifted = $this->storage->shift();
+        $this->assertSame('test', $shifted->name);
+        $this->assertCount(1, $this->storage);
+    }
+
+    public function testPopLastElementFromStorage()
+    {
+        $this->storage->push(new FakeInstance('test'));
+        $this->storage->push(new FakeInstance('another try'));
+        $this->storage->push(new FakeInstance('essai'));
         $poped = $this->storage->pop();
 
-        $this->assertInstanceOf(Essai::class, $poped);
+        $this->assertInstanceOf(FakeInstance::class, $poped);
         $this->assertSame('essai', $poped->name);
     }
 
     public function testGetAnyElementFromStorage()
     {
-        $this->storage->attach(new Essai('test premier'));
-        $this->storage->attach(new Essai('essai'));
-        $this->storage->attach(new Essai('another test'));
+        $this->storage->push(new FakeInstance('test premier'));
+        $this->storage->push(new FakeInstance('essai'));
+        $this->storage->push(new FakeInstance('another test'));
         $found = $this->storage->find(
-            fn (Essai $essai) => str_contains($essai->name, 'essai')
+            fn (FakeInstance $essai) => str_contains($essai->name, 'essai')
         );
 
-        $this->assertInstanceOf(Essai::class, $found);
+        $this->assertInstanceOf(FakeInstance::class, $found);
         $this->assertSame('essai', $found->name);
     }
 
     public function testFilteredElementsFromStorage()
     {
-        $this->storage->attach(new Essai('test premier'));
-        $this->storage->attach(new Essai('essai'));
-        $this->storage->attach(new Essai('another test'));
+        $this->storage->push(new FakeInstance('test premier'));
+        $this->storage->push(new FakeInstance('essai'));
+        $this->storage->push(new FakeInstance('another test'));
         $found = $this->storage->filter(
-            fn (Essai $essai) => str_contains($essai->name, 'test')
+            fn (FakeInstance $essai) => str_contains($essai->name, 'test')
         );
 
         $this->assertInstanceOf(Box::class, $found);
@@ -97,32 +126,44 @@ class BoxTest extends TestCase
     public function testDoSomethingWithEachElement()
     {
         $results = [];
-        $this->storage->attach(new Essai('test premier'));
-        $this->storage->attach(new Essai('essai'));
-        $this->storage->attach(new Essai('another test'));
+        $this->storage->push(new FakeInstance('test premier'));
+        $this->storage->push(new FakeInstance('essai'));
+        $this->storage->push(new FakeInstance('another test'));
         $this->storage->each(
-            function (Essai $essai) use (&$results) {
-                $results[] = $essai->capitalize();
+            function (FakeInstance $essai) use (&$results) {
+                $results[] = ucfirst($essai->name);
             }
         );
         $expected = [
-            'Test Premier',
+            'Test premier',
             'Essai',
-            'Another Test'
+            'Another test'
         ];
         $this->assertSame($expected, $results);
     }
 
+    public function testShouldReturnNewBoxOnMapCall()
+    {
+        $this->storage->push(new FakeInstance('test premier'));
+        $this->storage->push(new FakeInstance('essai'));
+        $this->storage->push(new FakeInstance('another test'));
+        $mapped = $this->storage->map(
+            fn (FakeInstance $essai) => new FakeInstance(strtoupper($essai->name))
+        );
+
+        $this->assertCount(3, $mapped->getIterator());
+        $this->assertSame('TEST PREMIER', $mapped->shift()->name);
+    }
+
     public function testGetArrayFromBox()
     {
-        $essai1 = new Essai('test premier');
-        $essai2 = new Essai('essai');
-        $essai3 = new Essai('another test');
-        $this->storage->attach($essai1);
-        $this->storage->attach($essai2);
-        $this->storage->attach($essai3);
+        $essai1 = new FakeInstance('test premier');
+        $essai2 = new FakeInstance('essai');
+        $essai3 = new FakeInstance('another test');
+        $this->storage->push($essai1);
+        $this->storage->push($essai2);
+        $this->storage->push($essai3);
         $expected = [$essai1, $essai2, $essai3];
         $this->assertSame($expected, $this->storage->toArray());
     }
-    */
 }
