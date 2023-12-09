4272e7949fe6123aa2a5988e6ea9333b7f2a827e
ascetik <ascetik@free.fr>
2023-12-09 18:12:53 +0100

    ComparisonResult doc

--- src/Types/ComparisonResult.php
+++ src/Types/ComparisonResult.php
@@ -1,7 +1,23 @@
 <?php
 
+/**
+ * This is part of the Ascetik objectstorage package
+ *
+ * @package    ObjectStorage
+ * @category   Enum
+ * @license    https://opensource.org/license/mit/  MIT License
+ * @copyright  Copyright (c) 2023, Vidda
+ * @author     Vidda <vidda@ascetik.fr>
+ */
+
+declare(strict_types=1);
+
 namespace Ascetik\ObjectStorage\Types;
 
+/**
+ * @abstract
+ * @version 1.0.0
+ */
 abstract class ComparisonResult
 {
     public function __construct(
@@ -10,5 +26,4 @@ abstract class ComparisonResult
     }
 
     abstract public function reversed(): bool;
-
 }
