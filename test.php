7468a03005173758ea898a79fcfbb5e506d5a53c
ascetik <ascetik@free.fr>
2023-12-10 10:26:10 +0100

    ReadableContainer, j'avais oublié un rewind...

--- test.php
+++ test.php
@@ -8,50 +8,92 @@ require 'vendor/autoload.php';
 // echo ('a' <=> 'b') . PHP_EOL;
 // echo strcmp('a', 'b') . PHP_EOL;
 
-class Number
+// class Number
+// {
+//     public function __construct(public readonly int $value)
+//     {
+//     }
+// }
+
+// $range = [40, 23, 10, 57, 27, 12, 34, 49, 20,57, 51, 69, 44];
+// $box = new Box();
+// foreach ($range as $key => $number) {
+//     echo $number . ' ';
+//     $box->push(new Number($number), $key);
+// }
+// echo PHP_EOL;
+
+// $box->sort(function (Number $a, Number $b) {
+//     return $a->value - $b->value;
+// });
+
+// echo 'résultat ASC : ' . PHP_EOL;
+
+// foreach ($box as $content) {
+//     echo $content->value;
+//     echo  ' ';
+//     // echo $box->content()->offsetGet($content).PHP_EOL;
+// }
+// echo PHP_EOL;
+
+// $box->clear();
+// foreach ($range as $key => $number) {
+//     // echo $number.' ';
+//     $box->push(new Number($number), $key);
+// }
+
+// $box->sort(
+//     fn (Number $a, Number $b) => $a->value - $b->value,
+//     BoxSortOrder::DESC
+// );
+
+// echo 'résultat DESC : ' . PHP_EOL;
+
+// foreach ($box as $content) {
+//     echo $content->value;
+//     echo  ' ';
+//     // echo $box->content()->offsetGet($content).PHP_EOL;
+// }
+echo PHP_EOL;
+echo 'essai avec des strings' . PHP_EOL;
+
+// echo strcmp('a', 'b') . PHP_EOL;
+// echo strcmp('abc', 'acd') . PHP_EOL;
+
+$names = [
+    'Richard', 'Willfried', 'Henri', 'Simon', 'Igor', 'Bernardine',
+    'Gontran', 'Thomas', 'Ernest', 'Zoé', 'Christophe',
+    'Daphné', 'Jennifer', 'Antoinette', 'Gérard', 'Bernard'
+];
+
+class User
 {
-    public function __construct(public readonly int $value)
+    public function __construct(public readonly string $name)
     {
     }
 }
 
-$range = [40, 23, 10, 57, 27, 12, 34, 49, 20,57, 51, 69, 44];
 $box = new Box();
-foreach ($range as $key => $number) {
-    echo $number . ' ';
-    $box->push(new Number($number), $key);
+foreach ($names as $name) {
+    echo $name . ' ';
+    $user = new User($name);
+    $box->push($user);
 }
 echo PHP_EOL;
 
-$box->sort(function (Number $a, Number $b) {
-    return $a->value - $b->value;
-});
+$sorting = function (User $a, User $b) {
+    return strcmp($a->name, $b->name);
+};
+$box->sort($sorting);
 
-echo 'résultat ASC : ' . PHP_EOL;
-
-foreach ($box as $content) {
-    echo $content->value;
-    echo  ' ';
-    // echo $box->content()->offsetGet($content).PHP_EOL;
+foreach ($box as $user) {
+    echo $user->name . ' ';
 }
 echo PHP_EOL;
 
-$box->clear();
-foreach ($range as $key => $number) {
-    // echo $number.' ';
-    $box->push(new Number($number), $key);
-}
+$box->sortReverse($sorting);
 
-$box->sort(
-    fn (Number $a, Number $b) => $a->value - $b->value,
-    BoxSortOrder::DESC
-);
-
-echo 'résultat DESC : ' . PHP_EOL;
-
-foreach ($box as $content) {
-    echo $content->value;
-    echo  ' ';
-    // echo $box->content()->offsetGet($content).PHP_EOL;
+foreach ($box as $user) {
+    echo $user->name . ' ';
 }
 echo PHP_EOL;
